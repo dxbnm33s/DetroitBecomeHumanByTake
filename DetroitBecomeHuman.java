f0b9b29e334f423a0c7c9ed8cddad076c0f0650b
大嶺慧剛 <67330020+dxbnm33s@users.noreply.github.com>
2020-07-25 02:23:11 +0900

    Add files via upload

---
+++ DetroitBecomeHuman.java
@@ -0,0 +1,603 @@
+public class DetroitBecomeHuman {
+	public static void main(String[] args) throws Exception {
+	
+	
+	System.out.print("初");
+	Thread.sleep(60);
+	System.out.print("め");
+	Thread.sleep(60);
+	System.out.print("ま");
+	Thread.sleep(60);	
+	System.out.print("し");
+	Thread.sleep(60);
+	System.out.print("て");
+	Thread.sleep(60);
+	System.out.println("。");
+	Thread.sleep(4000);
+	
+	
+	
+	
+	
+	System.out.print("私");
+	Thread.sleep(60);
+	System.out.print("の");
+	Thread.sleep(60);
+	System.out.print("名");
+	Thread.sleep(60);
+	System.out.print("前");
+	Thread.sleep(60);
+	System.out.print("は");
+	Thread.sleep(60);
+	System.out.print("、");
+	Thread.sleep(60);
+	System.out.print("L");
+	Thread.sleep(60);
+	System.out.print("u");
+	Thread.sleep(60);
+	System.out.print("c");
+	Thread.sleep(60);
+	System.out.print("a");
+	Thread.sleep(60);
+	System.out.print("s");
+	Thread.sleep(60);
+	System.out.print("で");
+	Thread.sleep(60);
+	System.out.print("す");
+	Thread.sleep(60);
+	System.out.println("。");
+	Thread.sleep(2000);
+	System.out.print("私");
+	Thread.sleep(60);
+	System.out.print("の");
+	Thread.sleep(60);
+	System.out.print("開");
+	Thread.sleep(60);
+	System.out.print("発");
+	Thread.sleep(60);
+	System.out.print("者");
+	Thread.sleep(60);
+	System.out.print("は");
+	Thread.sleep(60);
+	System.out.print("、");
+	Thread.sleep(60);
+	System.out.print("大");
+	Thread.sleep(60);
+	System.out.print("嶺");
+	Thread.sleep(60);
+	System.out.print("慧");
+	Thread.sleep(60);
+	System.out.print("剛");
+	Thread.sleep(60);
+	System.out.print("で");
+	Thread.sleep(60);
+	System.out.print("す");
+	Thread.sleep(60);
+	System.out.println("。");
+	Thread.sleep(2000);
+	System.out.print("私");
+	Thread.sleep(60);
+	System.out.print("の");
+	Thread.sleep(60);
+	System.out.print("年");
+	Thread.sleep(60);
+	System.out.print("齢");
+	Thread.sleep(60);
+	System.out.print("は");
+	Thread.sleep(60);
+	System.out.print(2);
+	Thread.sleep(60);
+	System.out.print(4);
+	Thread.sleep(60);
+	System.out.print("で");
+	Thread.sleep(60);
+	System.out.print("す");
+	Thread.sleep(60);
+	System.out.println("。");
+	Thread.sleep(2000);
+	
+
+	System.out.print("何");
+	Thread.sleep(60);	
+    System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("質");
+	Thread.sleep(60);	
+	System.out.print("問");
+	Thread.sleep(60);	
+	System.out.print("が");
+	Thread.sleep(60);	
+	System.out.print("あ");
+	Thread.sleep(60);	
+	System.out.print("れ");
+	Thread.sleep(60);	
+	System.out.print("ば");
+	Thread.sleep(60);	
+	System.out.print("聞");
+	Thread.sleep(60);	
+	System.out.print("い");
+	Thread.sleep(60);	
+	System.out.print("て");
+	Thread.sleep(60);	
+	System.out.print("く");
+	Thread.sleep(60);	
+	System.out.print("だ");
+	Thread.sleep(60);	
+	System.out.print("さ");
+	Thread.sleep(60);	
+	System.out.print("い");
+	Thread.sleep(60);	
+	System.out.print("ね");
+	Thread.sleep(60);	
+	System.out.print("。");
+		
+	System.out.println("");
+	Thread.sleep(2000);
+	
+	
+	System.out.print("何");
+	Thread.sleep(60);	
+	System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("質");
+	Thread.sleep(60);	
+	System.out.print("問");
+	Thread.sleep(60);	
+	System.out.print("は");
+	Thread.sleep(60);	
+	System.out.print("あ");
+	Thread.sleep(60);	
+	System.out.print("り");
+	Thread.sleep(60);	
+	System.out.print("ま");
+	Thread.sleep(60);	
+	System.out.print("す");
+	Thread.sleep(60);	
+	System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("？");
+	Thread.sleep(2000);	
+	System.out.println("");
+		
+	System.out.print("");
+	Thread.sleep(60);
+	
+	
+	String question = new java.util.Scanner(System.in).nextLine();
+	Thread.sleep(2000);
+	
+	
+	String question1 = "おはようございます";
+	
+	
+	switch (question1) {
+		case "おはようございます":
+			Thread.sleep(1000);
+			
+				System.out.print("お");
+				Thread.sleep(60);
+				System.out.print("は");
+				Thread.sleep(60);
+				System.out.print("よ");
+				Thread.sleep(60);
+				System.out.print("う");
+				Thread.sleep(60);
+				System.out.print("ご");
+				Thread.sleep(60);
+				System.out.print("ざ");
+				Thread.sleep(60);
+				System.out.print("い");
+				Thread.sleep(60);
+				System.out.print("ま");
+				Thread.sleep(60);
+				System.out.print("す");
+				Thread.sleep(60);
+				System.out.print("！");
+				Thread.sleep(60);
+				System.out.print("今");
+				Thread.sleep(60);
+				System.out.print("日");
+				Thread.sleep(60);
+				System.out.print("も");
+				Thread.sleep(60);
+				System.out.print("良");
+				Thread.sleep(60);
+				System.out.print("い");
+				Thread.sleep(60);
+				System.out.print("天");
+				Thread.sleep(60);
+				System.out.print("気");
+				Thread.sleep(60);
+				System.out.print("で");
+				Thread.sleep(60);
+				System.out.print("す");
+				Thread.sleep(60);
+				System.out.print("ね");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(1000);
+				System.out.println("");
+
+				
+			break;
+		default:
+			Thread.sleep(1000);
+				System.out.print("す");
+				Thread.sleep(60);
+				System.out.print("み");
+				Thread.sleep(60);
+				System.out.print("ま");
+				Thread.sleep(60);
+				System.out.print("せ");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(60);
+				System.out.print("言");
+				Thread.sleep(60);
+				System.out.print("っ");
+				Thread.sleep(60);
+				System.out.print("て");
+				Thread.sleep(60);
+				System.out.print("い");
+				Thread.sleep(60);
+				System.out.print("る");
+				Thread.sleep(60);
+				System.out.print("こ");
+				Thread.sleep(60);
+				System.out.print("と");
+				Thread.sleep(60);
+				System.out.print("が");
+				Thread.sleep(60);
+				System.out.print("よ");
+				Thread.sleep(60);
+				System.out.print("く");
+				Thread.sleep(60);
+				System.out.print("わ");
+				Thread.sleep(60);
+				System.out.print("か");
+				Thread.sleep(60);
+				System.out.print("り");
+				Thread.sleep(60);
+				System.out.print("ま");
+				Thread.sleep(60);
+				System.out.print("せ");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(1000);
+				System.out.println("");
+				break;
+			
+
+
+	}
+	System.out.print("他");
+	Thread.sleep(60);
+	System.out.print("に");
+	Thread.sleep(60);
+	System.out.print("質");
+	Thread.sleep(60);	
+	System.out.print("問");
+	Thread.sleep(60);	
+	System.out.print("は");
+	Thread.sleep(60);	
+	System.out.print("あ");
+	Thread.sleep(60);	
+	System.out.print("り");
+	Thread.sleep(60);	
+	System.out.print("ま");
+	Thread.sleep(60);	
+	System.out.print("す");
+	Thread.sleep(60);	
+	System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("？");
+	Thread.sleep(2000);	
+	System.out.println("");
+		
+	
+	
+	Thread.sleep(2000);
+	
+	
+	String questions = new java.util.Scanner(System.in).nextLine();
+	
+	
+	String question2 = "こんにちは";
+	switch (question2) {
+		case "こんにちは":
+			Thread.sleep(1000);
+				System.out.print("こ");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("に");
+				Thread.sleep(60);
+				System.out.print("ち");
+				Thread.sleep(60);
+				System.out.print("は");
+				Thread.sleep(60);
+				System.out.print("！");
+				Thread.sleep(60);
+				System.out.print("お");
+				Thread.sleep(60);
+				System.out.print("昼");
+				Thread.sleep(60);
+				System.out.print("寝");
+				Thread.sleep(60);
+				System.out.print("が");
+				Thread.sleep(60);
+				System.out.print("し");
+				Thread.sleep(60);
+				System.out.print("た");
+				Thread.sleep(60);
+				System.out.print("く");
+				Thread.sleep(60);
+				System.out.print("な");
+				Thread.sleep(60);
+				System.out.print("る");
+				Thread.sleep(60);
+				System.out.print("時");
+				Thread.sleep(60);
+				System.out.print("間");
+				Thread.sleep(60);
+				System.out.print("で");
+				Thread.sleep(60);
+				System.out.print("す");
+				Thread.sleep(60);
+				System.out.print("ね");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(1000);
+				System.out.println("");
+
+		break;
+		default:
+			Thread.sleep(1000);
+				System.out.print("す");
+				Thread.sleep(60);
+				System.out.print("み");
+				Thread.sleep(60);
+				System.out.print("ま");
+				Thread.sleep(60);
+				System.out.print("せ");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(60);
+				System.out.print("言");
+				Thread.sleep(60);
+				System.out.print("っ");
+				Thread.sleep(60);
+				System.out.print("て");
+				Thread.sleep(60);
+				System.out.print("い");
+				Thread.sleep(60);
+				System.out.print("る");
+				Thread.sleep(60);
+				System.out.print("こ");
+				Thread.sleep(60);
+				System.out.print("と");
+				Thread.sleep(60);
+				System.out.print("が");
+				Thread.sleep(60);
+				System.out.print("よ");
+				Thread.sleep(60);
+				System.out.print("く");
+				Thread.sleep(60);
+				System.out.print("わ");
+				Thread.sleep(60);
+				System.out.print("か");
+				Thread.sleep(60);
+				System.out.print("り");
+				Thread.sleep(60);
+				System.out.print("ま");
+				Thread.sleep(60);
+				System.out.print("せ");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(1000);
+				System.out.println("");
+	}
+	Thread.sleep(1000);
+	System.out.print("他");
+	Thread.sleep(60);
+	System.out.print("に");
+	Thread.sleep(60);
+	System.out.print("質");
+	Thread.sleep(60);	
+	System.out.print("問");
+	Thread.sleep(60);	
+	System.out.print("は");
+	Thread.sleep(60);	
+	System.out.print("あ");
+	Thread.sleep(60);	
+	System.out.print("り");
+	Thread.sleep(60);	
+	System.out.print("ま");
+	Thread.sleep(60);	
+	System.out.print("す");
+	Thread.sleep(60);	
+	System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("？");
+	Thread.sleep(2000);	
+	System.out.println("");
+		
+	System.out.print("");
+	
+	Thread.sleep(2000);
+	String questions2 = new java.util.Scanner(System.in).nextLine();
+	
+	
+	String question3 = "こんばんは";
+	switch (question3) {
+		case "こんばんは":
+				System.out.print("こ");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("ば");
+				Thread.sleep(60);
+				System.out.print("ん");
+				Thread.sleep(60);
+				System.out.print("は");
+				Thread.sleep(60);
+				System.out.print("！");
+				Thread.sleep(60);
+				System.out.print("日");
+				Thread.sleep(60);
+				System.out.print("が");
+				Thread.sleep(60);
+				System.out.print("暮");
+				Thread.sleep(60);
+				System.out.print("れ");
+				Thread.sleep(60);
+				System.out.print("て");
+				Thread.sleep(60);
+				System.out.print("き");
+				Thread.sleep(60);
+				System.out.print("ま");
+				Thread.sleep(60);
+				System.out.print("し");
+				Thread.sleep(60);
+				System.out.print("た");
+				Thread.sleep(60);
+				System.out.print("ね");
+				Thread.sleep(60);
+				System.out.print("。");
+				Thread.sleep(60);
+				System.out.print("今");
+				Thread.sleep(60);
+				System.out.print("日");
+				Thread.sleep(60);
+				System.out.print("１");
+				Thread.sleep(60);
+				System.out.print("日");
+				Thread.sleep(60);
+				System.out.print("ど");
+				Thread.sleep(60);
+				System.out.print("う");
+				Thread.sleep(60);
+				System.out.print("で");
+				Thread.sleep(60);
+				System.out.print("し");
+				Thread.sleep(60);
+				System.out.print("た");
+				Thread.sleep(60);
+				System.out.print("か");
+				Thread.sleep(60);
+				System.out.print("?");
+				Thread.sleep(1000);
+				System.out.println("");
+
+		break;
+		default:
+			System.out.println("すみません。言っていることがよくわかりません。");
+	}
+	System.out.print("他");
+	Thread.sleep(60);
+	System.out.print("に");
+	Thread.sleep(60);
+	System.out.print("質");
+	Thread.sleep(60);	
+	System.out.print("問");
+	Thread.sleep(60);	
+	System.out.print("は");
+	Thread.sleep(60);	
+	System.out.print("あ");
+	Thread.sleep(60);	
+	System.out.print("り");
+	Thread.sleep(60);	
+	System.out.print("ま");
+	Thread.sleep(60);	
+	System.out.print("す");
+	Thread.sleep(60);	
+	System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("？");
+	Thread.sleep(2000);	
+	System.out.println("");
+		
+	System.out.print("");
+	
+	Thread.sleep(2000);
+	String questions3 = new java.util.Scanner(System.in).nextLine();
+	
+	
+	String answer = "終了";
+		switch(answer) {
+			case "終了":
+				Thread.sleep(1000);
+	System.out.print("ま");
+	Thread.sleep(60);
+	System.out.print("た");
+	Thread.sleep(60);		
+	System.out.print("何");
+	Thread.sleep(60);	
+    System.out.print("か");
+	Thread.sleep(60);	
+	System.out.print("質");
+	Thread.sleep(60);	
+	System.out.print("問");
+	Thread.sleep(60);	
+	System.out.print("が");
+	Thread.sleep(60);	
+	System.out.print("あ");
+	Thread.sleep(60);	
+	System.out.print("れ");
+	Thread.sleep(60);	
+	System.out.print("ば");
+	Thread.sleep(60);	
+	System.out.print("聞");
+	Thread.sleep(60);	
+	System.out.print("い");
+	Thread.sleep(60);	
+	System.out.print("て");
+	Thread.sleep(60);	
+	System.out.print("く");
+	Thread.sleep(60);	
+	System.out.print("だ");
+	Thread.sleep(60);	
+	System.out.print("さ");
+	Thread.sleep(60);	
+	System.out.print("い");
+	Thread.sleep(60);	
+	System.out.print("ね");
+	Thread.sleep(60);	
+	System.out.print("。");
+		
+	System.out.println("");
+	Thread.sleep(4000);
+
+			break;
+			default:
+				System.out.println("すみません。言っていることがよく分かりません。");
+				break;
+		
+	}
+
+	}
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
